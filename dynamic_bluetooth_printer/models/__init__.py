846798da34c2c98b572473fd02026b8e0b93c7d6
Anchal <anchal06@naman-personal-odoo-testing.us-central1-c.c.coastal-height-450504-i4.internal>
2025-03-23 05:17:55 +0000

    removed the dynamic printer module

--- dynamic_bluetooth_printer/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import bluetooth_printer
-from . import bluetooth_printer_device
-from . import escpos_generator
-from . import bluetooth_printer_discovery
--- dynamic_bluetooth_printer/wizards/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import bluetooth_printer_scan_wizard
-from . import print_label_wizard
