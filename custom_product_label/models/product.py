d9847363a750771adb0d50d1e3993bd9590544de
Tanu <tanu12345@tanu-odoo-database.us-central1-c.c.coastal-height-450504-i4.internal>
2025-03-29 11:35:10 +0000

    added default location for the product

--- custom_product_label/models/product.py
+++ custom_product_label/models/product.py
@@ -3,14 +3,10 @@ from odoo import api, fields, models
 class ProductProduct(models.Model):
     _inherit = 'product.product'
     
-    # This field already exists in stock module, but we're ensuring it's available
-    property_stock_inventory = fields.Many2one(
-        'stock.location', 
-        string="Inventory Location",
-        company_dependent=True,
-        domain="[('usage', '=', 'inventory')]",
-        help="This location will be used as the destination location for inventory loss.")
-
+    def action_print_custom_label(self):
+        """Print custom label for this product"""
+        self.ensure_one()
+        return self.env.ref('custom_product_label.action_report_custom_product_label').report_action(self)
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
